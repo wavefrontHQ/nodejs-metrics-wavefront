561f71463621ef5f2190c0cf181a185fb4eb3f7a
howardyoo <32691630+howardyoo@users.noreply.github.com>
2017-12-01 23:59:52 -0800

    uploading the source code to my fork
    upload initial files

---
+++ metrics/meter.js
@@ -0,0 +1,73 @@
+var EWMA = require('../stats/exponentially_weighted_moving_average.js');
+/*
+*
+*/
+var Meter = module.exports = function Meter(tags) {
+  this.m1Rate = EWMA.createM1EWMA();
+  this.m5Rate = EWMA.createM5EWMA();
+  this.m15Rate = EWMA.createM15EWMA();
+  this.count = 0;
+  this.startTime = (new Date).getTime();
+  this.type = 'meter';
+  if (!isEmpty(tags)) {
+    this.tags = tags
+  }else{
+    this.tags = {}
+  }
+  if(!(Object.keys(this.tags).length === 0 && this.tags.constructor === Object)){
+    console.log('Meter tag string: %s', this.tags);
+  }
+}
+
+// Mark the occurence of n events
+Meter.prototype.mark = function(n) {
+  if (!n) { n = 1; }
+  this.count += n;
+  this.m1Rate.update(n);
+  this.m5Rate.update(n);
+  this.m15Rate.update(n);
+}
+
+Meter.prototype.rates = function() {
+  return {1: this.oneMinuteRate()
+      , 5: this.fiveMinuteRate()
+      , 15: this.fifteenMinuteRate()
+      , mean: this.meanRate()};
+}
+
+// Rates are per second
+Meter.prototype.fifteenMinuteRate = function() {
+  return this.m15Rate.rate();
+}
+
+Meter.prototype.fiveMinuteRate = function() {
+  return this.m5Rate.rate();
+}
+
+Meter.prototype.oneMinuteRate = function() {
+  return this.m1Rate.rate();
+}
+
+Meter.prototype.meanRate = function() {
+  return this.count / ((new Date).getTime() - this.startTime) * 1000;
+}
+
+Meter.prototype.printObj = function() {
+  return {type: 'meter'
+      , count: this.count
+      , m1: this.oneMinuteRate()
+      , m5: this.fiveMinuteRate()
+      , m15: this.fifteenMinuteRate()
+      , mean: this.meanRate()
+      , unit: 'seconds'};
+}
+
+Meter.prototype.tick = function(){
+  this.m1Rate.tick();
+  this.m5Rate.tick();
+  this.m15Rate.tick();
+}
+
+function isEmpty(value) {
+  return typeof value == 'string' && !value.trim() || typeof value == 'undefined' || value === null;
+}
