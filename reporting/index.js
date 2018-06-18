697a1e0d361f8e15893cc426b79d1ce0dac2508f
Vikram Raman <vikram@wavefront.com>
2018-06-18 16:12:46 -0700

    Remove unnecessary files

--- reporting/index.js
+++
@@ -1,7 +0,0 @@
-exports.Report = require('./report');
-exports.Server = require('./server');
-exports.ScheduledReporter = require('./scheduled-reporter');
-exports.ConsoleReporter = require('./console-reporter');
-exports.CsvReporter = require('./csv-reporter');
-exports.GraphiteReporter = require('./graphite-reporter');
-exports.WavefrontReporter = require('./wavefront-reporter');
