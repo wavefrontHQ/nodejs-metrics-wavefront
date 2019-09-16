965ed43303ad222c1f0c907ab0c95d5c999b901e
djia-vm-wf <54043925+djia-vm-wf@users.noreply.github.com>
2019-09-16 14:47:01 -0700

    init (#20)
    * init
    
    * Update helper.js
    
    * code review
    
    * BigInt

--- test/helper.js
+++ test/helper.js
@@ -27,3 +27,67 @@ describe('decodeKey', function() {
     expect(decodedKey[1]).to.be.equal(null);
   });
 });
+
+describe('encodeKey', function () {
+  it('String for tags', function () {
+    expect( function () {
+      helper.encodeKey("http.request", "{\"key1\":\"val1\"}");
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: String");
+  });
+});
+
+describe('encodeKey', function () {
+  it('Number for tags', function () {
+    expect( function () {
+      helper.encodeKey("http.request", 42);
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: Number");
+    expect( function () {
+      helper.encodeKey("http.request", Math.LN2);
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: Number");
+    expect( function () {
+      helper.encodeKey("http.request", Infinity);
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: Number");
+    expect( function () {
+      helper.encodeKey("http.request", NaN);
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: Number");
+  });
+});
+
+describe('encodeKey', function () {
+  it('Boolean for tags', function () {
+    expect( function () {
+      helper.encodeKey("http.request", true);
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: Boolean");
+  });
+});
+
+describe('encodeKey', function () {
+  it('Symbol for tags', function () {
+    expect( function () {
+      helper.encodeKey("http.request", Symbol());
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: Symbol");
+  });
+});
+
+describe('encodeKey', function() {
+  it('Undefined for tags', function() {
+    let key11 = helper.encodeKey('http.request', undefined);
+    expect(key11).to.be.equal('http.request');
+  });
+});
+
+describe('encodeKey', function () {
+  it('Array for tags', function () {
+    expect( function () {
+      helper.encodeKey("http.request", [1,2,3]);
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: Array");
+  });
+});
+
+describe('encodeKey', function () {
+  it('Regular Expression for tags', function () {
+    expect( function () {
+      helper.encodeKey("http.request", /regex/);
+    }).throw("Wrong Tags datatype sent to the API. Expected: Object. Actual: RegExp");
+  });
+});
